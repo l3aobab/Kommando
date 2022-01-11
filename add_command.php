3e980a6fbd25738b0790b0496927154974029392
l3aobab <noelonazo@gmail.com>
2022-01-11 11:39:59 +0100

    correccion de errores y formato a formulario
    Se corrigen errores en la hoja add_commando.php y se empieza a dar formato al formulario para añadir comandos

--- add_command.php
+++ add_command.php
@@ -7,49 +7,54 @@
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-	
-	<div class="cabecera">
-		<!--Nombre de la tabla-->
-		<div class="nombre">
-			<h2>Añadir un nuevo comando</h2>
-		</div>
-		<!--Boton para añadir un comando a la tabla-->
-	</div>
+	<div class="cuerpo2">
+		<div class="vacio1"></div>
+		<div class="muestra">
+			<div class="cabecera">
+				<!--Nombre de la tabla-->
+				<div class="nombre2">
+					<h2>Añadir un nuevo comando</h2>
+				</div>
+				<!--Boton para añadir un comando a la tabla-->
+			</div>
 
-	<div class="formulario1">
-		<!--Formulario para introducir un nuevo comando a una tabla-->
-		<form method="post" action="add_command.php">
-			<label>Vendor</label>
-			<input type="text" name="vendor"><br>
-			<label>Comando completo</label>
-			<input type="text" name="completo"><br>
-			<label>Uso</label>
-			<input type="text" name="uso"><br>
-			<input type="submit" name="add" value="Añadir">
-		</form>
-	</div>
+			<div class="formulario1">
+				<!--Formulario para introducir un nuevo comando a una tabla-->
+				<form method="post" action="add_command.php">
+					<label>Vendor</label>
+					<input type="text" name="vendor"><br>
+					<label>Comando completo</label>
+					<input type="text" name="completo"><br>
+					<label>Uso</label>
+					<input type="text" name="uso"><br>
+					<input type="submit" name="add" value="Añadir">
+				</form>
+			</div>
 
-	<div class="botones">
-		<!--Boton para volver al menu principal-->
-		<div class="menu1">
-			<form action="index.php" method="post">
-				<input type="submit" name="volver" value="Volver al menu principal" class="menu2">
-			</form>
-		</div>
+			<div class="botones">
+				<!--Boton para volver al menu principal-->
+				<div class="menu1">
+					<form action="index.php" method="post">
+						<input type="submit" name="volver" value="Volver al menu principal" class="menu2">
+					</form>
+				</div>
+			</div>
+		<div class="vacio2"></div>
 	</div>
 
 	<?php 
-		#hay que arreglar esto que no va ni a tiros
+		
 		$conexion=mysqli_connect('localhost','root','','glosario');
-
 		if ($conexion) {
-			if (isset($_POST['add'])) {
+			if (isset($_POST['add']) && isset($_POST['vendor']) && isset($_POST['uso'])  && isset($_POST['completo'])) {
+
 				$vendor=$_POST['vendor'];
 				$uso=$_POST['uso'];
 				$completo=$_POST['completo'];
 				$comprobacion=mysqli_query($conexion,"SHOW TABLES");
 
 				if ($comprobacion != FALSE) {
+
 					if ($vendor == "alcatel") {
 						$consulta1=mysqli_query($conexion,"INSERT INTO alcatel (uso,completo) VALUES ('".$uso."','".$completo."')");
 						
@@ -64,14 +69,6 @@
 					}
 				}
 			}
-
-			else {
-			echo "ja";
-		}
-		}
-
-		else {
-			echo "xd";
 		}
 
 	?>
